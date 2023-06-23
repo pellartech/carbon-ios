131c5b17734f935fe3652abdaa4dc77a352f7c95
Ashok <ashok@Ashoks-MacBook-Air.local>
2023-06-23 17:42:10 +0530

    Added: BSC test net

--- Client/Application/AppDelegate.swift
+++ Client/Application/AppDelegate.swift
@@ -182,7 +182,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         networks = CoreDataManager.shared.fetchNetworks()
         if (networks.count == 0){
             for network in NetworkEnum.allCases{
-                let network = CoreDataManager.shared.saveNetwork(name: network.rawValue,isTest: network.isTest(),nativeSymbol: network.nativeSymbol(), isDefault: network == NetworkEnum.BinanceSmartChain ? true : false)
+                let network = CoreDataManager.shared.saveNetwork(name: network.rawValue,isTest: network.isTest(),nativeSymbol: network.nativeSymbol(), isDefault: (network == NetworkEnum.BinanceSmartChain || network == NetworkEnum.BinanceSmartChainTest ) ? true : false)
                 networks.append(network)
                 CoreDataManager.shared.save()
             }
